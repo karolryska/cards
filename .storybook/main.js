4bef16f89f28a724e069e70ff654df57ca9bdcfa
Karol Ryska <karolryska@gmail.com>
2022-12-13 23:48:00 +0100

    chore: install addons

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,6 +4,8 @@ module.exports = {
         '@storybook/addon-links',
         '@storybook/addon-essentials',
         '@storybook/addon-interactions',
+        '@storybook/preset-scss',
+        'storybook-css-modules',
     ],
     framework: '@storybook/react',
     core: {
