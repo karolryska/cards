5148f40858cd3440bef9371b35e17b4f1dfa9043
Karol Ryska <karolryska@gmail.com>
2023-01-21 19:25:25 +0100

    add initial page for not authenticated users

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,34 @@
+import { useRouter } from 'next/router';
+import { Logo } from 'components/logo/logo';
+import { Button } from 'components/button/button';
+import styles from '../styles/pages/index.module.scss';
+
 const HomePage = () => {
-    return <h1>Hello, world</h1>;
+    const router = useRouter();
+
+    const handleSeeDemoClick = () => {
+        router.push('/dashboard');
+    };
+
+    return (
+        <div className={styles.wrapper}>
+            <h1 className={styles.title}>
+                <Logo />
+                <span className={styles.hidden}>cards</span>
+            </h1>
+            <span />
+            <Button disabled fullWidth onClick={() => null} type="button">
+                Log in
+            </Button>
+            <Button disabled fullWidth onClick={() => null} type="button">
+                Sign up
+            </Button>
+            <span />
+            <Button fullWidth onClick={handleSeeDemoClick} type="button">
+                See demo
+            </Button>
+        </div>
+    );
 };
 
 export default HomePage;
