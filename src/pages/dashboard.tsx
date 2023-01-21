3a169427a09d167355d92481115ff328e5567a0a
Karol Ryska <karolryska@gmail.com>
2023-01-21 01:00:07 +0100

    implement modal with add new collection form

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,5 +1,9 @@
+import { useState } from 'react';
 import { Layout } from 'components/layout/layout';
 import { Dashborad } from 'components/dashboard/dashboard';
+import { Modal } from 'components/modal/modal';
+import { Button } from 'components/button/button';
+import { Input } from 'components/input/input';
 
 const collections = [
     { id: '1', name: 'react' },
@@ -10,13 +14,40 @@ const collections = [
 ];
 
 const DashboardPage = () => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [newCollectionName, setNewCollectionName] = useState('');
+
     const handleCardClick = (value: string) => {
         console.log(value);
     };
 
+    const handleAddButtonClick = () => setIsModalOpen(true);
+
     return (
         <Layout>
-            <Dashborad heading="collections" items={collections} onCardClick={handleCardClick} />
+            <Dashborad
+                heading="collections"
+                items={collections}
+                onAddClick={handleAddButtonClick}
+                onCardClick={handleCardClick}
+            />
+            <Modal
+                closeOnClickOutside
+                isOpen={isModalOpen}
+                onClose={() => setIsModalOpen(false)}
+                title="Add new collection"
+            >
+                <Input
+                    label="collection name"
+                    name="cname"
+                    onChange={setNewCollectionName}
+                    type="text"
+                    value={newCollectionName}
+                />
+                <Button type="button" onClick={() => null}>
+                    Add
+                </Button>
+            </Modal>
         </Layout>
     );
 };
