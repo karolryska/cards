f05fc5f06ba87b71cc0ea1c777dd7ff8edeac2df
Karol Ryska <karolryska@gmail.com>
2023-01-21 01:01:00 +0100

    add attribute for testing purposes

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -7,7 +7,7 @@ interface CardProps {
 
 export const Card = ({ title, onClick }: CardProps) => {
     return (
-        <button className={styles.wrapper} onClick={() => onClick(title)}>
+        <button className={styles.wrapper} data-testid="card" onClick={() => onClick(title)}>
             <span className={styles.title}>{title}</span>
         </button>
     );
