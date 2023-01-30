63e2b1ea102859c569028ca7bcaa4c31d2a6075f
Karol Ryska <karolryska@gmail.com>
2023-01-30 22:23:03 +0100

    add card editor form component

---
+++ src/components/cardEditorForm/cardEditorForm.tsx
@@ -0,0 +1,32 @@
+import classNames from 'classnames/bind';
+import { Input } from 'components/input/input';
+import { ReactNode } from 'react';
+import styles from './cardEditorForm.module.scss';
+
+const cn = classNames.bind(styles);
+
+export type CardEditorFormProps = {
+    back: string;
+    buttons: ReactNode;
+    front: string;
+    setBack: (value: string) => void;
+    setFront: (value: string) => void;
+};
+
+export const CardEditorForm = ({
+    back,
+    buttons,
+    front,
+    setBack,
+    setFront,
+}: CardEditorFormProps) => {
+    return (
+        <form className={cn('wrapper')}>
+            <div className={cn('textFields')}>
+                <Input name="front" label="front" onChange={setFront} type="text" value={front} />
+                <Input name="back" label="back" onChange={setBack} type="text" value={back} />
+            </div>
+            <div className={cn('buttons')}>{buttons}</div>
+        </form>
+    );
+};
