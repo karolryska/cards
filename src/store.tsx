68d08cd06cd36a9cf60317cbda9c64f4554d7f20
Karol Ryska <karolryska@gmail.com>
2023-01-30 23:21:19 +0100

    add new action to add card to collection

--- src/store.tsx
+++ src/store.tsx
@@ -1,24 +1,40 @@
+/* eslint-disable no-param-reassign */
 import { create } from 'zustand';
 import { devtools } from 'zustand/middleware';
+import { immer } from 'zustand/middleware/immer';
 import { v4 as uuidv4 } from 'uuid';
 import { Collection } from 'root/types';
 
 interface State {
     collections: Collection[] | [];
     addCollection: (name: string) => void;
+    addCard: (collectionId: string, front: string, back: string) => void;
 }
 
 export const useCollections = create<State>()(
-    devtools((set) => ({
-        collections: [
-            { id: '1', name: 'foo', items: [{ id: '1', front: 'foo', back: 'bar' }] },
-            { id: '2', name: 'bar', items: [{ id: '1', front: 'foo', back: 'bar' }] },
-            { id: '3', name: 'baz', items: [{ id: '1', front: 'foo', back: 'bar' }] },
-        ],
-        addCollection: (name) =>
-            set((state) => {
-                const newCollection = { id: uuidv4(), name, items: [] };
-                return { collections: [...state.collections, newCollection] };
-            }),
-    })),
+    devtools(
+        immer((set) => ({
+            collections: [
+                { id: '1', name: 'foo', items: [{ id: '1', front: 'foo', back: 'bar' }] },
+                { id: '2', name: 'bar', items: [{ id: '1', front: 'foo', back: 'bar' }] },
+                { id: '3', name: 'baz', items: [{ id: '1', front: 'foo', back: 'bar' }] },
+            ],
+            addCollection: (name) =>
+                set((state) => {
+                    const newCollection = { id: uuidv4(), name, items: [] };
+                    return { collections: [...state.collections, newCollection] };
+                }),
+            addCard: (collectionId, front, back) =>
+                set((state) => {
+                    const newCard = { id: uuidv4(), front, back };
+                    const collectionIndex = state.collections.findIndex(
+                        ({ id }) => id === collectionId,
+                    );
+                    state.collections[collectionIndex].items = [
+                        ...state.collections[collectionIndex].items,
+                        newCard,
+                    ];
+                }),
+        })),
+    ),
 );
