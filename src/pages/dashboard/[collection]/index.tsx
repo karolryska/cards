c9efc7ce7dbc0a14da37ba6feaa255b06558ded2
Karol Ryska <karolryska@gmail.com>
2023-01-31 00:05:28 +0100

    add dynamic page with collection cards

---
+++ src/pages/dashboard/[collection]/index.tsx
@@ -0,0 +1,36 @@
+import { useRouter } from 'next/router';
+import { Layout } from 'components/layout/layout';
+import { Dashborad } from 'components/dashboard/dashboard';
+import { useCollections } from 'store';
+
+const CollectionPage = () => {
+    const router = useRouter();
+    const {
+        query: { collection: collectionName },
+    } = useRouter();
+
+    const cards = useCollections(
+        (state) =>
+            state.collections
+                .find((item) => item.name === collectionName)
+                ?.items.map(({ id, front }) => {
+                    return {
+                        id,
+                        name: front,
+                    };
+                }) || [],
+    );
+
+    return (
+        <Layout>
+            <Dashborad
+                heading={collectionName}
+                items={cards}
+                buttonLabel="Start quiz"
+                onAddClick={() => router.push(`${collectionName}/add`)}
+            />
+        </Layout>
+    );
+};
+
+export default CollectionPage;
