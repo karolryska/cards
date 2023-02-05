3304e9b7dd6fd1f1ee351e4a6cd443ca02b299b4
Karol Ryska <karolryska@gmail.com>
2023-02-05 15:48:54 +0100

    update tests because of props change and add test for submit function

--- src/components/cardEditorForm/cardEditorForm.test.tsx
+++ src/components/cardEditorForm/cardEditorForm.test.tsx
@@ -1,30 +1,55 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { CardEditorForm } from './cardEditorForm';
 
 describe('card editor form', () => {
-    beforeEach(() => {
+    const before = (initialCardValues?: { front: string; back: string }) => {
         render(
             <CardEditorForm
-                front="front value"
-                back="back value"
-                setFront={() => null}
-                setBack={() => null}
-                buttons={<button>add</button>}
+                buttons={[{ label: 'submit', onClick: () => null, type: 'submit' }]}
+                card={initialCardValues || { front: '', back: '' }}
+                onSubmit={() => null}
             />,
         );
-    });
+    };
 
     test('should render textareas with passed front and back card values', async () => {
+        before({ front: 'front value', back: 'back value' });
+
         expect((screen.getByLabelText('front') as HTMLTextAreaElement).value).toBe('front value');
         expect((screen.getByLabelText('back') as HTMLTextAreaElement).value).toBe('back value');
     });
 
     test('should render passed button', async () => {
+        before();
+
         expect(
             screen.getByRole('button', {
-                name: /add/i,
+                name: /submit/i,
             }),
         );
+    });
+
+    test('should trigger onSubmit function on button click', async () => {
+        const handleSubmitMock = jest.fn((front, back) => {
+            return { front, back };
+        });
+
+        render(
+            <CardEditorForm
+                buttons={[{ label: 'submit', onClick: () => null, type: 'submit' }]}
+                card={{ front: 'front value', back: 'back value' }}
+                onSubmit={handleSubmitMock}
+            />,
+        );
+
+        fireEvent(
+            screen.getByRole('button', {
+                name: /submit/i,
+            }),
+            new MouseEvent('click'),
+        );
+
+        expect(handleSubmitMock).toHaveBeenCalledWith('front value', 'back value');
     });
 });
