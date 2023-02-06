f97150b4a05e0ecdf6b7367b3032852a5a1f4015
Karol Ryska <karolryska@gmail.com>
2023-02-06 22:47:02 +0100

    add context with hook to store device size

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
-import '../styles/main.scss';
+import { DeviceSizeContextProvider } from 'context/deviceWidth';
 import type { AppProps } from 'next/app';
+import '../styles/main.scss';
 
 export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    return (
+        <DeviceSizeContextProvider>
+            <Component {...pageProps} />
+        </DeviceSizeContextProvider>
+    );
 }
